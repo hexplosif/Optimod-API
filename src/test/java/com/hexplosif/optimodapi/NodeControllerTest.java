4e7540e8a416635a66fcf4b6d5d5470d36f3f57d
lkusno <97843576+howdrox@users.noreply.github.com>
2024-12-02 11:48:55 +0100

    tests

--- src/test/java/com/hexplosif/optimodapi/NodeControllerTest.java
+++ src/test/java/com/hexplosif/optimodapi/NodeControllerTest.java
@@ -8,10 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
